a97bf35f4a48153c30e0c1f0627251a198486d28
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-08 05:24:39 -0700

    print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-/**
- * main - prints to standard error
+/** main - prints to standard error
  *
  * Return: one
  */
-int main(void)
-{
-	fprintf(stderr, "and that piece of art is useful\"
-		       	- Dora Korpar, 2015-10-19\n");
-	return (1);
+int main(void) {
+       
+	fprintf(stderr, "and that piece of art is useful\" -  Dora
+		Korpar, 2015-10-19\n"); 
+	return (1); 
 }
