4a764bd24048f5de2a58a9dc3ef0e6e58ebc65a3
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-09 02:30:22 -0700

    prints out combinations 0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print possible cobinations 0-9
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+		if (i == 9)
+			break;
+		putchar(",");
+		putchar(" "):
+	}
+	putchar("\n");
+	return (0);
