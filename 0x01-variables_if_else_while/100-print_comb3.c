e773e65b91a4f9a0c79ccd9063bb7438925d691b
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-09 05:50:40 -0700

    Prints 00 to 99 without variable char

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,18 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i <= 99; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (i == 99)
-			break;
-		putchar(',');
-		putchar(' ');
+		for (j = 1; j <= 99; j++)
+		{
+			putchar(ij);
+			if (i == j)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
