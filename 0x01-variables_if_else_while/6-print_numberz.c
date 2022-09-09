faad1c1a6491da239ea13781bbb86a53fbee2ae0
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-09 01:58:17 -0700

    print single digit number of base 10 from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers of base 10 from 0
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
