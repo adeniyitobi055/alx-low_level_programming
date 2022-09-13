3dc80f57ea95d702cc5db2739d5e60d3fdfb3411
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-13 02:48:00 -0700

    Prints sum two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "math.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first integer
+ * @b: second interger
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum)
+}
