96b2c95ec29c99b96af6a8d8f319cc1fe1c9e6cf
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-13 07:18:27 -0700

    Print fibonacci numbers from 0-50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1 = 0;
+	int n2 = 1;
+	int n3;
+	int n;
+
+	for (n = 3; n <= 50; n++)
+	{
+		putchar(n1);
+		putchar(n2);
+		n3 = n1 + n2;
+		putchar(n3);
+
+		n1 = n2;
+		n2 = n3;
+	}
+}
