68c5f197ff803fb8ee5002e030095b1ed673fc85
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-13 01:20:05 -0700

    Aplhabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic alphabet
+ *
+ * @c: parameter
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && , <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
