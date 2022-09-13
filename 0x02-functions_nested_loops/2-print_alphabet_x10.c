4de4e91de00b2d105371022dbbd455a4bbf4dc50
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-13 00:58:22 -0700

    Prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = 'a';  j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
