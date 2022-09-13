f6c3076964e1f1964d4586d953ad744929c25de7
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-13 02:55:34 -0700

    Prints all natural numbers from 0-98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural numbers
+ *
+ * @n: parameter
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		if (n == 98)
+			break;
+		_putchar(',');
+		_putchar(' ');
+	}
+	return (0);
+}
