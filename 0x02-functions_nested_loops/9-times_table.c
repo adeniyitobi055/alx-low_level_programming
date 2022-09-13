4b84c04c1ef32c80d6b2c709d6ef19ffc34927b5
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-13 04:28:49 -0700

    Prints 9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,6 +18,10 @@ void times_table(void)
 
 			_putchar(',');
 			_putchar(' ');
+			(result <= 9);
+			_putchar(' ');
+			_putchar('0' + (result / 10));
+			_putchar('0' + (result % 10));
 		}
 	}
 	_putchar('\n');
