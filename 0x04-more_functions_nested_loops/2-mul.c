76b33670ed2b6203487e62d63785f72302e74c14
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-15 00:20:01 -0700

    Print the multiple of two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	_putchar("%d\n", m);
+}
