29d9ffada9b04f0c0c411320a06e9da13f9a0965
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-16 02:39:45 -0700

    Prints line on terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,13 @@
  * @n: number of times to print _.
  */
 
-void print_line(int n);
+void print_line(int n)
 {
-	if (n > 0)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
-		n--;
-	}
-	else
-	{
-		_putchar('\n');
+		_putchar(95);
 	}
 	_putchar('\n');
 }
