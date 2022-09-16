f858b214c0e7ea09218ed68deeab3ceee1e28b26
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-16 00:54:39 -0700

    print 10 times 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -22,10 +22,11 @@ void more_numbers(void)
 			else
 			{
 				_putchar('0' + j / 10);
-				_putchar('0' + j % 10 '\n');
+				_putchar('0' + j % 10);
 			}
 			j++;
 		}
+		_putchar('\n');
 		i++;
 	}
 }
