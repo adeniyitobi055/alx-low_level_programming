4cf26e9e3fbdb5362c7332a692448d1c4575e7bb
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-19 06:12:58 -0700

    Prints string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,34 +9,10 @@
 void print_rev(char *s)
 {
 	int i;
-	/*int length = strlen(string);
-	int middle = length / 2;*/
 
 	for (i = 100; s[i]; i--)
 	{
-		/*s = string[i];
-		string[i] = string[length - i - 1];
-		string[length -i -1] = s;*/
 		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
-
-/**
- * reverse - swap a string
- * Return: swapped string s
- * @string: pointer
- *
-
-void reverse(char *string)
-{
-	int length = strlen(string);
-	int middle = length / 2;
-	char temp;
-	for (int i = 0; i < middle; i++)
-	{
-		temp = string[i];
-		string[i] = string[length - i - 1];
-		string[length - i - 1] = temp;
-	}
-}*/
