a0bba9fb947a8ef38e6d20cf9a53539bc918c03f
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-19 03:43:00 -0700

    Reset value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - rest value to 98
+ * @n: value to be rest to
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n: %d\n", n);
+	reset_to_98(n);
+	print("n: %d\n", n);
+	return (0);
+}
