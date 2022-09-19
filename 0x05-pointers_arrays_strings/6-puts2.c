285bf40dbf46785e082e615a8f2c8cd5fe9ae67b
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-19 07:02:00 -0700

    prints evenly

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - print evenly
+ * @str: parameter
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = strlen(str) - 1; i >= 0; i+2)
+	{
+		_putchar(str[i]);
+	}
+}
