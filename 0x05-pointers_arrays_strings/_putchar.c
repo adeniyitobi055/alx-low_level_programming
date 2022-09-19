96cdf2a6855338ed07b655a8443e64bc1e55e4ba
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-19 04:55:42 -0700

    String

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - check the code
+ *
+ * @str: parameter
+ * Return: Always 0.
+ */
+int _putchar(char str)
+{
+	return (write(1, &str, 1));
+}
