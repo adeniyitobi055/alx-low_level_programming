86e1a87ac0b60edd7945ba9938fa10464bec8cb4
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-19 06:36:50 -0700

    Reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: string to be reversed
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = s[10]; i < s[10]; i--)
+	{
+		
+	}
+	return (i);
+}
