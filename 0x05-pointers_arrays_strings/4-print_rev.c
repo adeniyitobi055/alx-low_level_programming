1fd11474b724ce4471b48fc79de5721388315873
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-19 05:06:38 -0700

    Prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = '\0'; s[i]; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
