ef41f73ddfb4067ee9bb5a820eb272f7a71dc7c9
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-21 07:44:15 -0700

    Functions

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_odd(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
