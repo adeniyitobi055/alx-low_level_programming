a7bb73e69ebac9507dbc58bd327c913b93804b5d
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-22 00:41:11 -0700

    Infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -27,7 +27,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	for (; *n1; n1--, r_index--)
 	{
 		num = (*n1 - '0') + tens;
-		*(r + r_index) = (num % 10) +'0';
+		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 	for (; *n2; n2--, r_index--)
@@ -58,15 +58,15 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int index, n1_len = 0, n2_len = 0;
-	
+
 	for (index = 0; *(n1 + index); index++)
 		n1_len++;
 	for (index = 0; *(n2 + index); index++)
 		n2_len++;
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
-	n1 += n1_len -1;
-	n2 += n2_len -1;
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
 	return (add_strings(n1, n2, r, --size_r));
 }
