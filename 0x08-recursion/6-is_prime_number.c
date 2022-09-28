015ceee676dffa5e246b47dd31500afa7d7507c8
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-09-28 05:03:20 -0700

    Checks if its prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_prime_number - determine if number is prime or not
+ * @n: Integer
+ *
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	i = 0;
+
+	if (i < n)
+	{
+		if (n % (n - i) == 0)
+			return (1);
+	}
+	return (0);
+}
