805ef9798d27259753e24dea33691c269ae9072f
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-10-12 05:25:19 -0700

    Prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,8 @@
  * print_name - Function to prints a name.
  * @name: character to be printed.
  * @f: function pointer.
- *
  * Return: Nothing.
- */
+**/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
