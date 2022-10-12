19ca5763b6c9f777a35c1bc21eb9fbb9ebf64e2f
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-10-12 05:28:55 -0700

    Performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print result of an operation.
+ * @argc: number of arguments.
+ * @argv: argument characters.
+ * Return: Return an int.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((strcmp(argv[2], "+")) && (strcmp(argv[2], "-")) &&
+		(strcmp(argv[2], "*")) && (strcmp(argv[2], "/")) &&
+		(strcmp(argv[2], "%")))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+		&& atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - Adds two integers.
+ * @a: Integer 1.
+ * @b: Integer 2.
+ * Return: Returns result.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two integers.
+ * @a: Integer 1.
+ * @b: Integer 2.
+ * Return: Returns result.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers.
+ * @a: Integer 1.
+ * @b: Integer 2.
+ * Return: Returns result.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two integers.
+ * @a: Integer 1.
+ * @b: Integer 2.
+ * Return: Returns result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modules two integers.
+ * @a: Integer 1.
+ * @b: Integer 2.
+ * Return: Returns result.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
