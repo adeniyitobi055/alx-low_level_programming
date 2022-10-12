b7fb48ae2f27d53812f92608e6515a227ad5a579
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-10-12 05:56:11 -0700

    Performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	}
 
 	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-		&& atoi(argv[3]) == 0)
+		|| atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(98);
