b8e22e49c7f59e7d82f3a891c90242ca0e062e28
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-10-20 23:41:55 -0700

    Prints to standard output before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void start(void) __attribute__ ((constructor));
+
+/**
+ * start - print to stdout before main
+ * function is executed.
+ */
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
