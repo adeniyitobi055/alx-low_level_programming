66218ee577c9d64a7ceaa410d19e07a93c335306
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-10-25 01:45:30 -0700

    Delete a node at index of a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,6 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index != 0)
 	{
 		unsigned int i = 0;
+
 		while (i < index - 1 && temp != NULL)
 		{
 			temp = temp->next;
