8ca86ee6c2bce8c7beaec7e0ec2421491db0dfcf
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-12-08 03:22:05 -0800

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,6 +3,8 @@
 /**
  * dlistint_len - returns number of elements in a list.
  * @h: pointer to the list.
+ *
+ * Return: Number of elements in the list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
