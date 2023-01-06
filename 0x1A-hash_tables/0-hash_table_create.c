47a0d6f74695c67cfd8a7d2b2f4e6fa0d4105a5d
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-01-06 07:02:29 -0800

    create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: size of the array.
+ *
+ * Return: A pointer to the newly created hash table
+ * or NULL if failed.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (!size)
+		return (NULL);
+
+	ht = calloc(1, sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
+}
